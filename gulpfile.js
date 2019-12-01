df888b6ae7ffb25eee1bc276b60d2af895bcd3de
唐门 <179292705@qq.com>
2019-12-01 19:21:02 +0800

    使用gulp压缩静态文件

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
+var imagemin = require('gulp-imagemin');
 var babel = require('gulp-babel');
 
 // 压缩css文件
@@ -39,6 +40,35 @@ gulp.task('minify-js', function (done) {
     done();
 });
 
+// 压缩 public/images 目录内图片(Version<3)
+// gulp.task('minify-images', function () {
+//     gulp.src('./public/images/**/*.*')
+//         .pipe(imagemin({
+//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+//             progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+//             interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+//             multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
+//         }))
+//         .pipe(gulp.dest('./public/images'));
+// });
+
+// 压缩 public/images 目录内图片(Version>3)
+gulp.task('minify-images', function (done) {
+    gulp.src('./public/images/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./public/images'));
+    done();
+});
 
 //4.0以前的写法 
 //gulp.task('default', [
@@ -46,7 +76,7 @@ gulp.task('minify-js', function (done) {
 //]);
 //4.0以后的写法
 // 执行 gulp 命令时执行的任务
-gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css', 'minify-js')), function () {
+gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css', 'minify-js', 'minify-images')), function () {
     console.log("----------gulp Finished----------");
     // Do something after a, b, and c are finished.
 });
