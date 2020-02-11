ecd4e4e061b6a61e1034714fea0be0dbcd774067
tiexo <179292705@qq.com>
2020-02-11 14:45:58 +0800

    icarus主题更新至3.0.0

---
+++ themes/icarus3/layout/common/comment.jsx
@@ -0,0 +1,29 @@
+const logger = require('hexo-log')();
+const { Component } = require('inferno');
+const view = require('hexo-component-inferno/lib/core/view');
+
+module.exports = class extends Component {
+    render() {
+        const { config, page, helper } = this.props;
+        const { __ } = helper;
+        const { comment } = config;
+        if (!comment || typeof comment.type !== 'string') {
+            return null;
+        }
+
+        return <div class="card">
+            <div class="card-content">
+                <h3 class="title is-5">{__('article.comments')}</h3>
+                {(() => {
+                    try {
+                        const Comment = view.require('comment/' + comment.type);
+                        return <Comment config={config} page={page} helper={helper} comment={comment} />;
+                    } catch (e) {
+                        logger.w(`Icarus cannot load comment "${comment.type}"`);
+                        return null;
+                    }
+                })()}
+            </div>
+        </div>;
+    }
+};
