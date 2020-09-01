e06500a9326167339e4ec58a79f1fd59a56b2c63
tiexo <179292705@qq.com>
2020-09-01 12:45:11 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,10 @@ var babel = require('gulp-babel');
 gulp.task('minify-css', function() {
     var option = {
         rebase: false,
-        //advanced: true,               //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+        advanced: true,               //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
         compatibility: '*',         //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
-        //keepBreaks: true,             //类型：Boolean 默认：false [是否保留换行]
-        //keepSpecialComments: '*'      //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        keepBreaks: true,             //类型：Boolean 默认：false [是否保留换行]
+        keepSpecialComments: '*'      //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
     }
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
